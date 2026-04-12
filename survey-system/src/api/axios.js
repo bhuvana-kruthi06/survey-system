97b47243376ef262d66fd17dafb177341d0eaad5
bhuvana-kruthi06 <kruthichalamalla06@gmail.com>
2026-04-12 16:17:28 +0530

    updated API URL to render backend

--- survey-system/src/api/axios.js
+++ survey-system/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://survey-system-backend-x9wk.onrender.com/api",
 });
 
 export default API;
